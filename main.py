c7cec26d8cb40a92050bb21a72714f4af96a9deb
jueun0716 <1213jhje@gmail.com>
2024-05-05 17:56:53 -0700

    기능 추가
    삭제, 수정 기능 추가

--- main.py
+++ main.py
@@ -19,4 +19,21 @@ def create_memo(memo:Memo):
 def read_memo():
     return memos
 
+@app.put("/memo/{memo_id}") #메모를 id로 받는다.
+def put_memo(req_memo:Memo): #요청한 메모랑 memo 랑 맞는지 확인
+    for memo in memos: #memos를 돌린다.
+        if memo.id == req_memo.id:
+            memo.content = memo.content
+            return '성공했습니다.'      
+    return '그런 메모는 없습니다.' #for 문이 실패했을때
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id): 
+    for index,memo in enumerate (memos) : #포문을 쓸때 인덱스를 쓰려면 배열 뒤에 enumerate로 감싸줘야한다. enumerate는 배열에 인덱스와 같이 뽑아주는 명령어
+        if memo.id==memo_id: #path로 전달해주는거랑 아이디를 찾아서 
+            memos.pop(index) #pop 없앤다.
+            return '성공했습니다.'      
+    return '그런 메모는 없습니다.' #for 문이 실패했을때
+            
+
 app.mount("/", StaticFiles(directory="static", html=True),name="static")
--- static/app.js
+++ static/app.js
@@ -1,7 +1,45 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id; //서버에 맞는 아이디를 요청 하고 수정할 수 있게 만드는
+  const editInput = prompt("수정할 값을 입력하세요"); //수정된 값을 input 해라
+  const res = await fetch(`/memo/${id}`, {
+    method: "PUT", //어떤 값을 수정할때는 PUT 을 특정 값이 있을때 그 값으로 바꿔주라는 의미
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memo/${id}`, {
+    method: "DELETE", //특정 메모를 삭제
+  });
+  readMemo();
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
+
   const li = document.createElement("li");
-  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  li.innerText = `${memo.content}`; //[id:${memo.id}] 앞에 id 값 표시
+
+  const editBtn = document.createElement("button"); //메모 옆에 버튼
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id; //메모에 edit를 넣어줌
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
+  li.appendChild(delBtn);
+  li.appendChild(editBtn);
   ul.appendChild(li);
 }
 
